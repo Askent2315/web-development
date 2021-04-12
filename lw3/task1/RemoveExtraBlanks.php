0a9820248acda928f2776f4e582b280ed1fe3274
pasha-1215 <pasha-1215@mail.tu>
2021-04-12 09:05:11 +0300

    Лабы

--- lw3/task1/RemoveExtraBlanks.php
+++ lw3/task1/RemoveExtraBlanks.php
@@ -1,10 +1,13 @@
 <?php
+  header('Content-Type: text/plain');
   if ($_GET['text'] <> '')
     $str = $_GET['text'];
   else
+  {
     $str = 'invalid text';
+    exit;
+  }
   $str = preg_replace('/\s+/', ' ', $str);
   $str = trim($str, ' '); 
-  header('Content-Type: text/plain');
   echo $str;
 ?>
